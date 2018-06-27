e8f931cbfe3038c751daae18d63147188d610c45
Nhân <40637767+dungladeongo@users.noreply.github.com>
2018-06-27 23:41:52 +0700

    update auth + view

--- routes/web.php
+++ routes/web.php
@@ -158,3 +158,7 @@ Route::group(['prefix' => 'dathang'],function (){
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
